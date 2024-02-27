3e7d938ecadccfea8a40d7bcdf8553fec6696ae9
gasperapi <gasperapi@gmail.com>
2024-02-27 22:18:02 +0700

    layout nav + foot

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,10 +11,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-1">{children}</main>
-      <Footer />
-    </div>
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <div className="flex-1">{children}</div>
+          <Footer />
+        </div>
+      </body>
+    </html>
   );
 }
